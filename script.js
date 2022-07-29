677575aee96cc975d28d9578544c80b0506b74ac
Jeffrey <jeffreywong0417@gmail.com>
2022-07-29 18:17:03 -0400

    Added changes

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 // Global Variables
 
-const DEFAULT_BLACK = "#000000";
 const DEFAULT_WHITE = "#FFFFFF"
 const BRUSH = 'brush';
 const ERASER = 'eraser';
 
-let currentColor = DEFAULT_BLACK;
 let currentTool = BRUSH; // initial tool set to brush
 let isMouseDown = false;
+
+let size = 8;
 
 const board = document.querySelector('.board-container');
 const brushButton = document.querySelector('#brush');
 const eraserButton = document.querySelector('#eraser');
 const clearButton = document.querySelector('#clear');
+const colorpicker = document.querySelector('#colorpicker');
+const sizeSlider = document.querySelector('#size-slider');
+const sizeText = document.querySelector('.size-text');
 
 brushButton.onclick = () => {currentTool = BRUSH;}
 eraserButton.onclick = () => {currentTool = ERASER;}
@@ -20,11 +23,11 @@ document.body.onmousedown = () => {isMouseDown = true}
 document.body.onmouseup = () => {isMouseDown = false}
 
 function createBoard() {
-    for (let i = 0; i < 256; i++) {
+    for (let i = 0; i < size * size; i++) {
         const cell = document.createElement('div');
         cell.className = 'cell';
         cell.id = 'cell-' + String(i);
-        cell.style = 'width: 29.25px; height: 29.25px;' // board height/width = 500px => 500 / 16 = 31.25px - 2px bc of border so 29.25px
+        cell.style = 'width:' + String((500 / size) - 2) + 'px; height: ' + String((500 / size) - 2) + 'px' // board height/width = 500px => 500 / 16 = 31.25px - 2px bc of border so 29.25px
 
         cell.addEventListener('mouseover', changeColorCell)
 
@@ -34,22 +37,32 @@ function createBoard() {
     }
 }
 
-
 function changeColorCell(e) {
     if (e.type === 'mouseover' && !isMouseDown) {
         return;
     }
     if (currentTool === BRUSH) {
-        e.target.style.backgroundColor = currentColor;
+        e.target.style.backgroundColor = colorpicker.value;
     }
     if (currentTool === ERASER) {
         e.target.style.backgroundColor = DEFAULT_WHITE;
     }
 }
 
+function editGridSize() {
+    board.innerHTML = '';
+    size = sizeSlider.value;
+    createBoard();
+}
+
 clearButton.onclick = () => {
     board.innerHTML = '';
     createBoard();
+}
+
+sizeSlider.oninput = () => {
+    sizeText.innerHTML = String(sizeSlider.value) + 'x' + String(sizeSlider.value);
+    editGridSize();
 }
 
 
